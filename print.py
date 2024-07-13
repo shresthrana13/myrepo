1da8c713c0c70dfb628a7680f33c85bccbf2bbe6
shresth rana <shresthrana13@gmail.com>
2024-07-13 22:02:05 +0530

    this is 6th version

--- print.py
+++ print.py
@@ -2,4 +2,5 @@ print('''hello world
       my name is Shresth Rana
       i am looking for a job
       i will do my best to do soo to learn
-      and get a good job with merit''')
+      and get a good job with merit
+      and do good for family''')
